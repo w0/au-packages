bc6abb32585f4704ea77da0915ec439f67aeeedc
w0 <33212583+w0@users.noreply.github.com>
2021-01-09 14:12:19 -0600

    added oldschool-runescape

---
+++ oldschool-runescape/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.runescape.com/downloads/oldschool.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'msi'
+  url           = $url
+
+  softwareName  = 'OldSchool RuneScape Launcher*'
+
+  checksum      = '0BBFBA3444CABBF3F213598386A93BC4A7EF782157F900F01413883C153F10C0'
+  checksumType  = 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
