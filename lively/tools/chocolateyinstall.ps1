a317dd2c450a7ac1cde2d6c549a8fed8f0003152
w0 <33212583+w0@users.noreply.github.com>
2021-01-16 00:56:46 -0600

    added lively

---
+++ lively/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/rocksdanister/lively/releases/download/v1.1.8.0/lively_setup_x86_full_v1180.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  softwareName  = 'lively*'
+
+  checksum      = 'b9faf8638e178e98cdec421c869203255b832f1f17c5d6402db7ed69bb00d1fe'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ lively/update.ps1
@@ -0,0 +1,33 @@
+import-module au
+. $PSScriptRoot\..\_scripts\all.ps1
+
+$releases = 'https://github.com/rocksdanister/lively/releases'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*fileType\s*=\s*)('.*')"     = "`$1'$($Latest.FileType)'"
+
+        }
+    }
+}
+
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $re      = '\.exe$'
+    $url     = $download_page.links | Where-Object href -match $re | Select-Object -First 1 -expand href
+    $url     = 'https://github.com' + $url
+    $version = $url -split '/' | Select-Object -Last 1 -Skip 1
+
+    @{
+        Version      = $version.TrimStart('v')
+        URL32        = $url
+        ReleaseNotes = ''
+    }
+}
+
+update 
