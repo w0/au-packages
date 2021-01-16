093d8ba92dda4021fcbdcb4ac7b633ab27e2f5ab
w0 <33212583+w0@users.noreply.github.com>
2021-01-16 02:37:47 -0600

    [AU lively]

--- lively/tools/chocolateyinstall.ps1
+++ lively/tools/chocolateyinstall.ps1
@@ -14,7 +14,7 @@ $packageArgs = @{
   checksum      = 'b9faf8638e178e98cdec421c869203255b832f1f17c5d6402db7ed69bb00d1fe'
   checksumType  = 'sha256'
 
-  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /NODEPENDENCIES /NOAUTOLAUNCH'
   validExitCodes= @(0)
 }
 
