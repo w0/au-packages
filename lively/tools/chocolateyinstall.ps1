b55cc7098a3ca87a6c6a20cb978281034cf88ef6
w0 <33212583+w0@users.noreply.github.com>
2021-01-16 17:58:14 -0600

    [AU lively]

--- lively/tools/chocolateyinstall.ps1
+++ lively/tools/chocolateyinstall.ps1
@@ -8,23 +8,23 @@ $CurrentBuild      = [System.Environment]::OSVersion.Version.Build
 
 if ( $CurrentBuild -lt $MinBuildSupported ) {
   
-  throw 'Unsupport build of Winodws. Minimum support build is {0}. Please update to install.' -f $MinBuildSupported
+  Write-Output 'Unsupported build of Windows. Minimum supported build is {0}. Please update to avoid errors.' -f $MinBuildSupported
 
 }
 
-  $packageArgs = @{
-    packageName    = $env:ChocolateyPackageName
-    unzipLocation  = $toolsDir
-    fileType       = 'exe'
-    url            = $url
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'exe'
+  url            = $url
 
-    softwareName   = 'lively*'
+  softwareName   = 'lively*'
 
-    checksum       = 'b9faf8638e178e98cdec421c869203255b832f1f17c5d6402db7ed69bb00d1fe'
-    checksumType   = 'sha256'
+  checksum       = 'b9faf8638e178e98cdec421c869203255b832f1f17c5d6402db7ed69bb00d1fe'
+  checksumType   = 'sha256'
 
-    silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /ALLUSERS /NODEPENDENCIES /NOAUTOLAUNCH'
-    validExitCodes = @(0)
-  }
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /ALLUSERS /NODEPENDENCIES /NOAUTOLAUNCH'
+  validExitCodes = @(0)
+}
 
-  Install-ChocolateyPackage @packageArgs
+Install-ChocolateyPackage @packageArgs
