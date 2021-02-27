b432fa82798c6ad4c25353ace6fe253093387727
Chocolatey <chocolatey@realdimensions.net>
2021-02-27 11:59:39 +0000

    AU: 1 updated - runescape [skip ci] https://gist.github.com/a569e20fa01812b6a86bd6d0c3663614

--- runescape/tools/chocolateyinstall.ps1
+++ runescape/tools/chocolateyinstall.ps1
@@ -11,7 +11,7 @@ $packageArgs = @{
 
   softwareName  = 'RuneScape Launcher*'
 
-  checksum      = '8701fd8283d14bea410dc64641fc989d582308bee649183922a045d6872a26ea'
+  checksum      = 'ee39728c0175eb1f520e81fcad3a970d1bc7d1a471ea0a501f64860181448bc7'
   checksumType  = 'sha256'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
