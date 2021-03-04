1bbc38adf93d30fea1a958bc8fcfd0da94a1965b
Chocolatey <chocolatey@realdimensions.net>
2021-03-04 11:59:45 +0000

    AU: 1 updated - lively [skip ci] https://gist.github.com/a569e20fa01812b6a86bd6d0c3663614

--- lively/tools/chocolateyinstall.ps1
+++ lively/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url      = 'https://github.com/rocksdanister/lively/releases/download/v1.1.9.4/lively_setup_x86_full_v1194.exe'
+$url      = 'https://github.com/rocksdanister/lively/releases/download/v1.2.0.2/lively_setup_x86_full_v1202.exe'
 
 $MinBuildSupported = '18362'
 $CurrentBuild      = [System.Environment]::OSVersion.Version.Build
@@ -20,7 +20,7 @@ $packageArgs = @{
 
   softwareName   = 'lively*'
 
-  checksum       = '7db2435f037acef721213304c0e1084fa028d77a5ccce8710399053ad72226ad'
+  checksum       = '578de6d3ba8ce93ffc1c749e329d6b6764ac095ece4d979b312e1de9781409d9'
   checksumType   = 'sha256'
 
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /ALLUSERS /NODEPENDENCIES /NOAUTOLAUNCH'
