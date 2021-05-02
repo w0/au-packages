bc073dfb8b411256ad71b461f8d4f1f7437825c5
w0 <33212583+w0@users.noreply.github.com>
2021-05-01 19:11:42 -0500

    clean up

--- lively/tools/chocolateyinstall.ps1
+++ lively/tools/chocolateyinstall.ps1
@@ -19,12 +19,9 @@ $packageArgs = @{
   unzipLocation  = $toolsDir
   fileType       = 'exe'
   url            = 'https://github.com/rocksdanister/lively/releases/download/v1.4.0.0/lively_setup_x86_full_v1400.exe'
-
   softwareName   = 'lively*'
-
   checksum       = 'b07779164a9aa4803d270e67450de7e529e84a5ca37968900e82a204fa1d1d4f'
   checksumType   = 'sha256'
-
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /ALLUSERS /NODEPENDENCIES /NOAUTOLAUNCH'
   validExitCodes = @(0)
 }
--- runescape/tools/chocolateyinstall.ps1
+++ runescape/tools/chocolateyinstall.ps1
@@ -8,13 +8,10 @@ $packageArgs = @{
   unzipLocation = $toolsDir
   fileType      = 'exe'
   url           = $url
-
   softwareName  = 'RuneScape Launcher*'
-
   checksum      = 'ee39728c0175eb1f520e81fcad3a970d1bc7d1a471ea0a501f64860181448bc7'
   checksumType  = 'sha256'
-
-  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
 }
 
