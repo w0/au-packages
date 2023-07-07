491f49d4c57cac0849df1330d7777721265037f8
w0 <33212583+w0@users.noreply.github.com>
2023-07-06 22:58:14 -0500

    updated for url change.

--- files/update.ps1
+++ files/update.ps1
@@ -25,7 +25,7 @@ function global:au_GetLatest {
 
     @{
         Version      = $latest_release.tag_name.trim('v')
-        URL32          = 'https://files.community/appinstallers/Files.stable.appinstaller'
+        URL32          = 'https://files.community/appinstallers/Files.preview.appinstaller'
         ReleaseNotes = $latest_release.html_url
     }
 }
