ac60d9fdf5bf3348cb39d2d45a706d891e4531e0
Chocolatey <chocolatey@realdimensions.net>
2023-10-23 02:31:04 +0000

    AU: 1 updated - easyeda [skip ci] https://gist.github.com/w0/a569e20fa01812b6a86bd6d0c3663614/65c08e8f6792b65dc8b63d21d109a51aa136d328

--- easyeda/tools/chocolateyinstall.ps1
+++ easyeda/tools/chocolateyinstall.ps1
@@ -6,14 +6,14 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
-  url           = 'https://image.easyeda.com/files/easyeda-windows-ia32-6.5.36.exe'
-  url64bit      = 'https://image.easyeda.com/files/easyeda-windows-x64-6.5.36.exe'
+  url           = 'https://image.easyeda.com/files/easyeda-windows-ia32-6.5.37.exe'
+  url64bit      = 'https://image.easyeda.com/files/easyeda-windows-x64-6.5.37.exe'
 
   softwareName  = 'easyeda*'
 
-  checksum      = '19a1ed52a029aedf41575bd06c0a75b355afcda0ef7a07795830348fbac59045'
+  checksum      = '48e6f34f5ee3eecf5e88df7fdb477fe5bff15895374aa6022d9ba100b9103b47'
   checksumType  = 'sha256'
-  checksum64    = '1e78d2e748e5c491c2b1f56b82fecfdcd46834cf920fe8e55bfc41f1837daa74'
+  checksum64    = '9ee1ae2e4965f27eb1bc069c2444d0e4303590ebece9ac575e577c20ff077ce0'
   checksumType64= 'sha256'
   validExitCodes= @(0, 3010, 1641)
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
