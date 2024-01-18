6bebbcf2548adabcaffdd602255e8378b62e82aa
Chocolatey <chocolatey@realdimensions.net>
2024-01-18 14:31:19 +0000

    AU: 1 updated - logseq [skip ci] https://gist.github.com/w0/a569e20fa01812b6a86bd6d0c3663614/766098fdfb1eef2cc3ba67976077df6a98698524

--- logseq/tools/chocolateyinstall.ps1
+++ logseq/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $softwareName = 'Logseq'
-$version = '0.10.4'
+$version = '0.10.5'
 if ($version -eq (Get-UninstallRegistryKey "$softwareName").DisplayVersion) {
   Write-Host "Logseq $version is already installed."
   return
