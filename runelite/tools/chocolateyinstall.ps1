1de6bba45533b21748e9f88f10c016a4b0048d47
Chocolatey <chocolatey@realdimensions.net>
2024-01-31 18:30:58 +0000

    AU: 1 updated - runelite [skip ci] https://gist.github.com/w0/a569e20fa01812b6a86bd6d0c3663614/1a8374be8a001fbf903202044befb90673b9c84b

--- runelite/tools/chocolateyinstall.ps1
+++ runelite/tools/chocolateyinstall.ps1
@@ -6,12 +6,12 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
-  url           = 'https://github.com/runelite/launcher/releases/download/2.6.10/RuneLiteSetup32.exe'
-  url64         = 'https://github.com/runelite/launcher/releases/download/2.6.10/RuneLiteSetup.exe'
+  url           = 'https://github.com/runelite/launcher/releases/download/2.6.13/RuneLiteSetup32.exe'
+  url64         = 'https://github.com/runelite/launcher/releases/download/2.6.13/RuneLiteSetup.exe'
   softwareName  = 'runelite*'
-  checksum      = '4b3293e69c7e82e91be85c71e0d4eab072222aa3e46c01d2a52789aac355c604'
+  checksum      = 'ee9b11461b5aac6c134dcd468708bf488245fe89f8fdf72682c97179938b7abb'
   checksumType  = 'sha256'
-  checksum64    = '837370201e436f04cf94972757e18c294b05c30fd9d39aa0dd50513df6355af6'
+  checksum64    = '63a5a642080d78f063e6a7fecf2ec972227c7308fc6864c87bc106bde2cc7edb'
   checksumType64= 'sha256'
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES'
 }
