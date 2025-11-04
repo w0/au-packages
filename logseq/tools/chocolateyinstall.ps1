60b68d5f8df3e8fe9795c6370d6391805dd5644b
Chocolatey <chocolatey@realdimensions.net>
2025-11-04 04:04:43 +0000

    AU: 1 updated - logseq [skip ci] https://gist.github.com/w0/a569e20fa01812b6a86bd6d0c3663614/401ee5084f461de836436a4c5837b1bd8529ba11

--- logseq/tools/chocolateyinstall.ps1
+++ logseq/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $softwareName = 'Logseq'
-$version = '0.10.14'
+$version = '0.10.14.20251104'
 if ($version -eq (Get-UninstallRegistryKey "$softwareName").DisplayVersion) {
   Write-Host "Logseq $version is already installed."
   return
@@ -14,9 +14,9 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
-  url64         = ''
+  url64         = 'https://github.com/logseq/logseq/releases/download/0.10.14/Logseq-win-x64-0.10.14.exe'
   softwareName  = 'loqseq*'
-  checksum64    = ''
+  checksum64    = 'b77080300733e3cd693c46883c59a12025b1ec8ccaf76e1eadb016ee1f293299'
   checksumType64= 'sha256'
 }
 
